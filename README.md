342b3b19db3286e876448b9b99c5d9f0bff78e4a
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-12-12 00:08:17 +0100

    add a script, and update README

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# kakaotalk-env
+kakaotalk-env
+===
+
+This is the wrapper script to start and quit the Kakao Talk in the Debian Linux.
+
+## Preparation
+
+* Install Wine with root
+
+```
+dpkg --add-architecture i386 && apt-get update && apt-get install wine wine32 winbind
+```
+
+* Setup the basic Korean font for wine
+`emacs ~/.wine/system.reg`
+
+```
+"MS Shell Dlg"="NanumGothic"
+"MS Shell Dlg 2"="NanumGothic"
+```
+
+
+## Download the latest Kakao Talk for Windows
+
+https://downloadkakaotalk.com/kakao-talk-for-windows.html
+
+
+* Install the downloaded Kakao Talk with wine
+
+```
+wine KakaoTalk_Setup.exe
+```
+
+* Select **Korean**  even if the main window fonts are totally broken. One cannot see the text on buttons, but one should know which buttons are following:
+
+```
+1. Next
+2. Next
+3. Uncheck all > Install 
+4. Uncheck Run KakaoTalk > Finish
+```
+
+* Login with Kakao account
+
+* Go to the Settings, Select one of `Nanum` family fonts. The Kakao Talk will ask to restart it. 
+
+* Restart
+
+
+
+
+## Commands
+
+* Start it
+```
+bash kakaotalk.bash start
+```
+
+
+* Stop it
+```
+bash kakaotalk.bash start
+```
---
+++ kakaotalk.bash
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+#
+#  Copyright (c) 2019           Jeong Han Lee
+#
+#  The program is free software: you can redistribute
+#  it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 2 of the
+#  License, or any newer version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  this program. If not, see https://www.gnu.org/licenses/gpl-2.0.txt
+#
+#
+#   author  : Jeong Han Lee
+#   email   : jeonghan.lee@gmail.com
+#   date    : Wednesday, December 11 23:04:08 CET 2019
+#   version : 0.0.1
+
+declare -gr SC_SCRIPT="$(realpath "$0")"
+declare -gr SC_SCRIPTNAME=${0##*/}
+declare -gr SC_TOP="${SC_SCRIPT%/*}"
+declare -gr SC_LOGDATE="$(date +%y%m%d%H%M)"
+
+
+declare -gr ProgramFiles="${HOME}/.wine/drive_c/Program Files"
+declare -gr KakaoTalk="${ProgramFiles}/Kakao/KakaoTalk"
+
+EXIST=1
+NON_EXIST=0
+
+SUDO="sudo"
+
+function pushd { builtin pushd "$@" > /dev/null; }
+function popd  { builtin popd  "$@" > /dev/null; }
+
+
+function die
+{
+    error=${1:-1}
+    ## exits with 1 if error number not given
+    shift
+    [ -n "$*" ] &&
+	printf "%s%s: %s\n" "$scriptname" ${version:+" ($version)"} "$*" >&2
+    exit "$error"
+}
+
+
+## if [[ $(checkIfFile "${release_file}") -eq "$NON_EXIST" ]]; then
+#   NON_EXIT
+## fi
+
+function checkIfFile
+{
+    local file=$1
+    local result=""
+    if [ ! -e "$file" ]; then
+	result=$NON_EXIST
+	# doesn't exist
+    else
+	result=$EXIST
+	# exist
+    fi
+    echo "${result}"	 
+};
+
+
+function checkIfVar()
+{
+
+    local var=$1
+    local result=""
+    if [ -z "$var" ]; then
+	result=$NON_EXIST
+	# doesn't exist
+    else
+	result=$EXIST
+	# exist
+    fi
+    echo "${result}"
+}
+
+function get_ip
+{
+    local realip=$(ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n')
+
+    printf "Real IP address %s\n" "$realip"
+}
+
+
+function start_kakaotalk
+{
+    pushd "${KakaoTalk}"
+    wine KakaoTalk.exe &
+    popd
+}
+
+
+
+
+
+function stop_kakaotalk
+{
+    local pid=NON_EXIST;
+    pid=$(ps ax |grep KakaoTalk.exe | grep -v "grep" | awk '{print $1}')
+    if [[ $(checkIfVar "${pid}") -eq "$NON_EXIST" ]]; then
+	printf ">> Server is not running\n";
+    else
+	printf ">> Server is running with %s\n" "${pid}"
+	printf "   Killing the running server ....\n"
+	kill -9 ${pid}
+    fi
+}
+
+
+case "$1" in
+    start)
+	start_kakaotalk
+	;;
+    stop)
+	stop_kakaotalk
+	;;
+    restart)
+	stop_kakaotalk
+	start_kakaotalk
+	;;
+    ip)
+	get_ip;
+	;;
+     *)
+	echo "Usage: $0 {start|stop|restart|ip}"
+	exit 2
+esac
+
+exit
